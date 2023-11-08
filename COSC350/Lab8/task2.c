716d94faa7c900b9af01e6b5afdef3bf916c5fcb
Cole Barbes <cbarbes02@gmail.com>
2023-11-07 22:07:22 -0500

    fixed task2

--- COSC350/Lab8/task2.c
+++ COSC350/Lab8/task2.c
@@ -3,12 +3,13 @@
 #include <pthread.h>
 #define NUMTHREADS 4
 #define NUMINTS 20
+pthread_mutex_t mutex1 = PTHREAD_MUTEX_INITIALIZER;
 int *arr;
 int numscores;
 
 void err_sys(char *, int);
 void *thread1(void *arg);
-int findMedian();
+double findMedian();
 void *thread2(void *arg);
 void *thread3(void *arg);
 void *thread4(void *arg);
@@ -48,6 +49,8 @@ int main()
 		err_sys("Error: return code from pthread_create() is", rc);
 
 	pthread_join(threads[3], NULL);
+
+	free(arr);
 	exit(0);
 }
 
@@ -60,29 +63,33 @@ void err_sys(char *str, int msg)
 
 void *thread1(void *arg)
 {
-	//pthread_mutex_lock(&array_mutex);
+	pthread_mutex_lock(&mutex1);
+	arr = (int*)calloc(NUMINTS, sizeof(int));
 	numscores=0;
+	pthread_mutex_unlock(&mutex1);
 	int testscore = 0;
 	for(int i = 0; i<NUMINTS && testscore!=-1; i++){
 		printf("Please enter an integer: ");
 		scanf("%d", &testscore);
 		if(testscore != -1){
-			arr[i] = numscores;
+			pthread_mutex_lock(&mutex1);
+			arr[i] = testscore;
 			numscores++;
+			pthread_mutex_unlock(&mutex1);
 		}
-
 	}
-	//pthread_mutex_unlock(&array_mutex);
 	pthread_exit(NULL);
 }
 
-int findMedian()
+double findMedian()
 {
+	pthread_mutex_lock(&mutex1);
 	int size = numscores;
 	int array[size];
 	for(int i = 0; i<size; i++){
 		array[i] = arr[i];
 	}
+	pthread_mutex_unlock(&mutex1);
 	for(int i = 1; i<size; i++){
 		int key = array[i]; // current elemtent to be inserted
 		int j = i-1;
@@ -93,22 +100,26 @@ int findMedian()
 		}
 		array[j+1] = key;
 	}
+	for(int i = 0; i<size; i++)
+		printf("%d ", array[i]);
+	printf("\n");	
 	if(size %2 !=0)
 		return array[size/2];
 	else
-		return (array[(size/2)]+array[((size/2)+1)] / 2);
+		return ((array[(size/2)-1]+array[(size/2)]) / 2.00);
 }
 void *thread2(void *arg)
 {
-	int average = 0;
-	int median = 0;
-	int size = numscores;
-	for(int i = 0; i<size; i++){
+	double average = 0;
+	double median = 0;
+	pthread_mutex_lock(&mutex1);
+	for(int i = 0; i<numscores; i++){
 		average += arr[i];
 	}
-	average /= size;
+	average = average/numscores;
+	pthread_mutex_unlock(&mutex1);
 	median = findMedian();
-	printf("The average is: %d The median is: %d\n", average, median);
+	printf("The average is: %.2f The median is: %.2f\n", average, median);
 	pthread_exit(NULL);
 }
 
@@ -118,9 +129,9 @@ void *thread3(void *arg)
 
 	int min = 0, max = 0;
 	for(int i = 1; i<size; i++){
-		if(max < arr[i])
+		if(arr[max] < arr[i])
 			max = i;
-		if(min > arr[i])
+		if(arr[min] > arr[i])
 			min = i;
 	}
 	printf("Here are is the minimum: %d Here is the maximum: %d\n", arr[min], arr[max]);
@@ -129,16 +140,10 @@ void *thread3(void *arg)
 
 void *thread4(void *arg)
 {
-	for(int i = 0; i<numscores; i++){
-		printf("%d ", arr[i]);
-	}
-	printf("\n");
-
 	for(int i = 0; i<NUMINTS; i++){
 		arr[i] = 0;
 		printf("%d ", arr[i]);
 	}
 	printf("\n");
-
 	pthread_exit(NULL);
 }
